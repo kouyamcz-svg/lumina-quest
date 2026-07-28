d5a2653c4c9d23f46cf0072c15d4f54f65449473
kouyamcz-svg <kouya.mcz@gmail.com>
2026-07-28 13:05:39 +0900

    chore(LQ3): 音楽もキャッシュ対象に

--- lq3/sw.js
+++ lq3/sw.js
@@ -1,7 +1,7 @@
 // ルミナクエストIII サービスワーカー
 // assets.js は ないようが かわらない かぎり キャッシュから かえす（ロジックだけの こうしんを かるくする）
 const CACHE='lq3-v2';
-const ASSETS=['./','./index.html','./assets.js'];
+const ASSETS=['./','./index.html','./assets.js'];   // field.mp3 は はじめて つかう ときに キャッシュ
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
 });
@@ -14,7 +14,7 @@ self.addEventListener('fetch',e=>{
   if(e.request.method!=='GET') return;
   const url=new URL(e.request.url);
   // そざいは キャッシュゆうせん（おおきくて かわらない）
-  if(url.pathname.endsWith('/assets.js')){
+  if(url.pathname.endsWith('/assets.js') || url.pathname.endsWith('/field.mp3')){
     e.respondWith(caches.match(e.request).then(r=>r || fetch(e.request).then(res=>{
       const copy=res.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy)); return res;
     })));
