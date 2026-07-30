8e72612aff6979e854d5f99fc81bcf79d661dd1a
kouyamcz-svg <kouya.mcz@gmail.com>
2026-07-30 21:13:11 +0900

    chore(LQ3): ボスBGMもキャッシュ対象に

--- lq3/sw.js
+++ lq3/sw.js
@@ -15,7 +15,7 @@ self.addEventListener('fetch',e=>{
   if(e.request.method!=='GET') return;
   const url=new URL(e.request.url);
   // そざいは キャッシュゆうせん（おおきくて かわらない）
-  if(url.pathname.endsWith('/assets.js') || url.pathname.endsWith('/field.mp3') || url.pathname.endsWith('/castle.mp3') || url.pathname.endsWith('/town.mp3') || url.pathname.endsWith('/battle3.mp3') || url.pathname.endsWith('/dungeon.mp3')){
+  if(url.pathname.endsWith('/assets.js') || url.pathname.endsWith('/field.mp3') || url.pathname.endsWith('/castle.mp3') || url.pathname.endsWith('/town.mp3') || url.pathname.endsWith('/battle3.mp3') || url.pathname.endsWith('/dungeon.mp3') || url.pathname.endsWith('/boss.mp3')){
     e.respondWith(caches.match(e.request).then(r=>r || fetch(e.request).then(res=>{
       const copy=res.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy)); return res;
     })));
